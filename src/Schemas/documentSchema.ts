5ae4f54408784794e3e5bb5a35c3a2305097d02d
dbshankar2001 <bhima.devarakonda@aknamed.com>
2024-05-30 22:55:43 +0530

    made changes to document schema. Added fields like createdBy, createdAt, participants, deletedBy, deletedAt, admins, isPersisted and along with this added some middleware from mongoose which will be executed after saving the object to database.

--- src/Schemas/documentSchema.ts
+++ src/Schemas/documentSchema.ts
@@ -1,16 +1,80 @@
-import mongoose, { Document, Schema, Types } from "mongoose";
-import {ICharacter, Character} from './characterSchema'
+import mongoose, { Document, Schema, Types, PopulatedDoc } from "mongoose";
+import {ICharacter} from './characterSchema'
+import { IUser } from './userSchema'
+
+export interface Participant extends Document {
+  userToSiteId: Map<Types.ObjectId, number>;
+}
+
+const ParticipantSchema = new Schema({
+  userToSiteId: {
+    type: Map,
+    of: Number,
+    default: new Map()
+  }
+});
 
 export interface IDocument extends Document {
-  content: ICharacter[]
+  content: PopulatedDoc<ICharacter>[],
+  createdBy: PopulatedDoc<IUser>,
+  createdAt: string,
+  participants: Participant[],
+  deletedBy: PopulatedDoc<IUser>,
+  deletedAt: string,
+  admins: PopulatedDoc<IUser>[],
+  isPersisted: boolean
 }
 
 export const documentSchema = new Schema<IDocument>({
   content: [{
-    type: Character.schema,
+    type: Types.ObjectId,
     ref: "Character",
     default: ''
-  }]
+  }],
+  createdBy: {
+    type: Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  createdAt: {
+    type: String
+  },
+  participants: [{
+    type: ParticipantSchema
+  }],
+  deletedBy: {
+    type: Types.ObjectId,
+    ref: "User"
+  },
+  deletedAt: {
+    type: String
+  },
+  admins: [{
+    type: Types.ObjectId,
+    ref: "User"
+  }],
+  isPersisted: {
+    type: Boolean,
+    default: false
+  }
 })
+
+documentSchema.post<IDocument>('save', function() {
+  if(!this.isPersisted) {
+    const date = new Date();
+    this.createdAt = date.toLocaleDateString() + '-' + date.toLocaleTimeString();
+    this.isPersisted = true;
+  }
+})
+
+documentSchema.methods.addUser = function(user: IUser){
+  this.participants.set(user._id, this.participants.size + 1)
+  user.documentsParticipantIn.push(this._id)
+}
+
+documentSchema.methods.addAdmin = function(user: IUser) {
+  this.admins.push(user)
+  user.documentsParticipantIn.push(this._id)
+}
 
 export const Documents = mongoose.model<IDocument>('Document', documentSchema);
