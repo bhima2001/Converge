53630d6b893d28122d5f1ca0cf2357dfd4b8e943
dbshankar2001 <bhima.devarakonda@aknamed.com>
2024-06-05 21:38:31 +0530

    minor changes

--- src/helper/operationHelper.ts
+++ src/helper/operationHelper.ts
@@ -1,6 +1,6 @@
 const MAX_VALUE = 2147483647;
 
-const generateRandomValue = (min: number, max: number): number => {
+export const generateRandomValue = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
@@ -25,10 +25,8 @@ export const generateKey = (
   const newKey: number[] = [];
   let i: number = 0;
   while (true) {
-    const leftDigit: number =
-      leftDigitList[i] !== undefined ? leftDigitList[i] : 0;
-    const rightDigit: number =
-      rightDigitList[i] !== undefined ? rightDigitList[i] : MAX_VALUE;
+    const leftDigit: number = leftDigitList[i] ?? 0;
+    const rightDigit: number = rightDigitList[i] ?? MAX_VALUE;
     if (rightDigit - leftDigit > 1) {
       newKey.push(generateRandomValue(leftDigit + 1, rightDigit - 1));
       break;
