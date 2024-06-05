1824ae9e9ec611910fbce8b44208a7f12967d56a
dbshankar2001 <bhima.devarakonda@aknamed.com>
2024-06-05 21:33:15 +0530

    if userName or email is present in the req.session then through a middleware setting the user to req.user

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -1,13 +1,19 @@
+import { IUser, User } from "../Schemas/userSchema";
 import { requestType, responseType, nextFunctionType } from "../types";
 
-export const userSession = (
+export const userSession = async (
   req: requestType,
   res: responseType,
   next: nextFunctionType
 ) => {
   if (!req.session.isLoggedIn) {
     res.status(403).send("You are not authorized to access this page.");
-    res.redirect("/login");
+    return;
   }
+  const user: IUser | null = await User.findOne({ email: req.session.email });
+  if (!user) {
+    throw new Error("User not found with email: " + req.session.email);
+  }
+  req.user = user;
   next();
 };
